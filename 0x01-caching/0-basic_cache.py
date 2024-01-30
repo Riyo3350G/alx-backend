b52f9a078cc18c23fe3f8e716f7cd412747a21c2
riyo3350g <yechouben@gamail.com>
2024-01-30 19:54:28 +0100

    Task 0 Fix

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -13,6 +13,4 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """get method"""
-        if key is None or key not in self.cache_data.keys():
-            return None
-        return self.cache_data.get(key)
+        return self.cache_data.get(key, None)
