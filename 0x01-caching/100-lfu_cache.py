2fc44a4aa19339b2c4655d0039582e05627d1774
riyo3350g <yechouben@gamail.com>
2024-01-31 18:35:23 +0100

    Task 4 ADV Fix 0

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -12,6 +12,7 @@ class LFUCache(BaseCaching):
         super().__init__()
         self.cache_data = OrderedDict(self.cache_data)
         self.freq = {}
+        self.access_counter = 0
 
     def put(self, key, item):
         """put method"""
@@ -19,19 +20,22 @@ class LFUCache(BaseCaching):
             if key in self.cache_data.keys():
                 self.cache_data[key] = item
                 self.freq[key] += 1
+                self.access_counter += 1
                 self.cache_data.move_to_end(key, last=True)
             else:
-                if len(self.cache_data) >= super().MAX_ITEMS:
-                    recent_item = list(self.freq.keys())[0]
+                self.cache_data[key] = item
+                self.freq[key] = 1
+                self.access_counter += 1
+                if len(self.cache_data) > super().MAX_ITEMS:
+                    recent_item = min(self.freq, key=self.freq.get)
                     self.cache_data.pop(recent_item)
                     self.freq.pop(recent_item)
                     print("DISCARD: {}".format(recent_item))
-                self.cache_data[key] = item
-                self.freq[key] = 1
 
     def get(self, key):
         """get method"""
         if key in self.cache_data.keys():
             self.freq[key] += 1
+            self.access_counter += 1
             self.cache_data.move_to_end(key, last=True)
         return self.cache_data.get(key)
