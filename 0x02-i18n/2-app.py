393505e5e8e7b3765e1af306772c80eba7f847ab
riyo3350g <yechouben@gamail.com>
2024-02-06 12:13:55 +0100

    Add Task 2 Req

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" Basic Flask app Module """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """" Babel Config class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ Determine the best match for supported languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Display Hello world! """
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
